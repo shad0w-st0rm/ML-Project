ea133964edbbff6ca9ba8ec47cd448eee6dfac1d
Tanmaye Goel <tanmayegoel@gmail.com>
2024-12-09 15:13:05 -0600

    added deep learning using word2vec and lstm, added a gitignore file to ignore the word2vec library

--- main.py
+++ main.py
@@ -11,12 +11,21 @@ from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+import gensim
+import gensim.downloader as api
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Embedding, Dropout
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+import numpy as np
 import csv
 
+
 # Download NLTK data
-nltk.download('stopwords')
-nltk.download('wordnet')
-nltk.download('omw-1.4')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
 
 # Instantiate stemmer and lemmatizer
 stemmer = PorterStemmer()
@@ -24,6 +33,10 @@ lemmatizer = WordNetLemmatizer()
 
 # Define stopwords
 stop_words = set(stopwords.words('english'))
+
+# Load pre-trained model
+# word2vec = api.load('word2vec-google-news-300')
+word2vec = gensim.models.KeyedVectors.load("word2vec-google-news-300.kv")
 
 # Define the cleaning function
 def clean_text(text):
@@ -69,7 +82,7 @@ def clean_text(text):
     words = [word for word in words if word not in stop_words]
 
     # Apply stemming
-    words = [stemmer.stem(word) for word in words]
+    # words = [stemmer.stem(word) for word in words]
 
     # Apply lemmatization
     words = [lemmatizer.lemmatize(word) for word in words]
@@ -79,27 +92,168 @@ def clean_text(text):
     
     return cleaned_text
 
-# Define dataset files (one file per dataset)
-dataset_files = ["financial_dataset.csv", "hotel_dataset.csv", "movie_dataset.csv"]
+def vectorize_words(text):
+    # Tokenize the text into words
 
+    vector_list = []
+    words = text.split()
+    for word in words:
+        if word in word2vec.key_to_index:
+            vector_list.append(word2vec[word])
+    return vector_list
+
+def binary_lstm_model(length):
+    model_lstm = Sequential()
+
+    model_lstm.add(LSTM(128, input_shape=(length, word2vec.vector_size), return_sequences=False))
+
+    model_lstm.add(Dropout(0.2))
+
+    model_lstm.add(Dense(2, activation='sigmoid'))
+
+    model_lstm.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model_lstm
+
+def multiclass_lstm_model(length, n_classes):
+    model_lstm = Sequential()
+
+    model_lstm.add(LSTM(128, input_shape=(length, word2vec.vector_size), return_sequences=True))
+
+    model_lstm.add(LSTM(64, return_sequences=False))
+    model_lstm.add(Dropout(0.2))
+
+    model_lstm.add(Dense(n_classes, activation='softmax'))
+
+    model_lstm.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model_lstm
+
+def read_movie_csv():
+    y = []
+    sentence_vectors = []
+    
+    # Load and preprocess data
+    with open("movie_dataset.csv", "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            cleaned_line = clean_text(line[0])
+            word_vectors = vectorize_words(cleaned_line)
+            sentence_vectors.append(word_vectors)
+            y.append(0 if line[1] == 'negative' else 1)
+    return (sentence_vectors, y, 2)
+
+def read_financial_csv():
+    y = []
+    sentence_vectors = []
+    
+    # Load and preprocess data
+    with open("financial_dataset.csv", "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            cleaned_line = clean_text(line[0])
+            word_vectors = vectorize_words(cleaned_line)
+            sentence_vectors.append(word_vectors)
+            y.append(int(line[1]))
+    return (sentence_vectors, y, 3)
+
+def read_hotel_csv():
+    y = []
+    sentence_vectors = []
+    
+    # Load and preprocess data
+    with open("hotel_dataset.csv", "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            text_portion = line[:-1]
+            text = ""
+            for segment in text_portion:
+                text += segment
+            
+            cleaned_line = clean_text(text)
+            word_vectors = vectorize_words(cleaned_line)
+            sentence_vectors.append(word_vectors)
+            y.append(int(line[-1]) - 1)
+    return (sentence_vectors, y, 5)    
+
+def deep_learn_binary():
+    sentence_vectors, y, _ = read_movie_csv()
+    average_size = 0
+    for sentence in sentence_vectors:
+        average_size += len(sentence)
+    average_size /= len(sentence_vectors)
+    max_len = int(average_size)
+
+    y = np.array(y)
+    padded_sequences = pad_sequences(sentence_vectors, padding='post', dtype='float32', maxlen=max_len)
+
+    # Split data into training and validation sets
+    X_train, X_valid, y_train, y_valid = train_test_split(padded_sequences, y, test_size=0.2, random_state=42)
+
+    model_lstm = binary_lstm_model(max_len)
+    model_lstm.fit(X_train, y_train, epochs=10, batch_size=128)
+
+    y_pred_lstm = model_lstm.predict(X_valid)
+    y_pred_lstm = np.round(y_pred_lstm)
+    print(accuracy_score(y_valid, y_pred_lstm))
+
+def deep_learn_multiclass(csv_func):
+    sentence_vectors, y, num_classes = csv_func
+    average_size = 0
+    for sentence in sentence_vectors:
+        average_size += len(sentence)
+    average_size /= len(sentence_vectors)
+    max_len = int(average_size)
+
+    y = np.array(y)
+    y = to_categorical(y, num_classes=num_classes)
+    padded_sequences = pad_sequences(sentence_vectors, padding='post', dtype='float32', maxlen=max_len)
+
+    # Split data into training and validation sets
+    X_train, X_valid, y_train, y_valid = train_test_split(padded_sequences, y, test_size=0.2, random_state=42)
+
+    model_lstm = multiclass_lstm_model(max_len, num_classes)
+    model_lstm.fit(X_train, y_train, epochs=10, batch_size=512, use_multiprocessing=True)
+
+    y_pred_lstm = model_lstm.predict(X_valid)
+    y_pred_lstm = np.round(y_pred_lstm)
+    print(accuracy_score(y_valid, y_pred_lstm))
+
+def deep_learn(dataset):
+    if dataset == 'movies':
+        deep_learn_binary()
+    elif dataset == 'hotels':
+        deep_learn_multiclass(read_hotel_csv)
+    elif dataset == 'financial':
+        deep_learn_multiclass(read_financial_csv)
+
+deep_learn('movies')
+    
+
+'''
 # Function to process and train models for each dataset
 def process_and_train(file_path):
     X, y = [], []
+    sentence_vectors = []
     
     # Load and preprocess data
     with open(file_path, "r", encoding="utf-8") as file:
         reader = csv.reader(file)
         for line in reader:
-            X.append(clean_text(line[0]))
+            cleaned_line = clean_text(line[0])
+            word_vectors = vectorize_words(cleaned_line)
+            sentence_vectors.append(word_vectors)
             y.append(line[1])
     
+    padded_sequences = pad_sequences(sentence_vectors, padding='post', dtype='float32', maxlen=50)
+
     # Split data into training and validation sets
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Vectorize the data using TF-IDF
-    vectorizer = TfidfVectorizer()
-    X_train = vectorizer.fit_transform(X_train)
-    X_valid = vectorizer.transform(X_valid)
+    # vectorizer = TfidfVectorizer()
+    # X_train = vectorizer.fit_transform(X_train)
+    # X_valid = vectorizer.transform(X_valid)
 
     # Train and evaluate models
     results = {}
@@ -130,6 +284,9 @@ def process_and_train(file_path):
 
     return results
 
+# Define dataset files (one file per dataset)
+dataset_files = ["financial_dataset.csv", "hotel_dataset.csv", "movie_dataset.csv"]
+
 # Run the process for each dataset file and print results
 for i, file_path in enumerate(dataset_files, start=1):
     print(f"Dataset {i}: {file_path}")
@@ -137,3 +294,4 @@ for i, file_path in enumerate(dataset_files, start=1):
     for model, accuracy in results.items():
         print(f"{model}: {accuracy:.4f}")
     print("-" * 50)
+'''
